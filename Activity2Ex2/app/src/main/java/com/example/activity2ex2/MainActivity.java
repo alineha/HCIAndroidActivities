ad675942b4be87f525bf3e836a31272c20530f73
Aline Amorim <ahamorim@inf.ufrgs.br>
2021-09-07 00:45:48 -0300

    feat: implement activity 2 exercises 1/2

---
+++ Activity2Ex2/app/src/main/java/com/example/activity2ex2/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.activity2ex2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor light, temperature;
+    TextView lightText, temperatureText;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lightText = findViewById(R.id.lightTextView);
+        temperatureText = findViewById(R.id.temperatureTextView);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        temperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        if(temperature != null) {
+            sensorManager.registerListener(MainActivity.this, temperature,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else
+        {
+            temperatureText.setText("Temperature sensor not supported :(");
+        }
+
+        if(light != null) {
+            sensorManager.registerListener(MainActivity.this, light,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else
+        {
+            lightText.setText("Light sensor not supported :(");
+        }
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        if(sensor.getType() == Sensor.TYPE_LIGHT)
+        {
+            lightText.setText(Float.toString(event.values[0]));
+        }
+        else
+            if (sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE){
+                temperatureText.setText(Float.toString(event.values[0]));
+            }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+}
